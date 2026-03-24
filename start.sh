d7301370c3b456a7a4cda33ba0da7c42257d4eef
odaniellevi <daniellevi.unicap@gmail.com>
2026-03-24 02:41:35 -0300

    removendo Dockerfile, .dockerignore e start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Baixar recursos do NLTK
-python -c "import nltk; nltk.download('punkt'); nltk.download('punkt_tab')"
-
-# Iniciar o servidor
-gunicorn backend:app --timeout 120 --workers 1
